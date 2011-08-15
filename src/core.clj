4ee98f4428950358e766b49c0db4236f937ca553
Alex Baranosky <alexander.baranosky@gmail.com>
2011-08-15 19:31:33 -0400

    reshaped code to prepare a spot for the fucntion we need, to get Google to give us the list of urls

--- src/core.clj
+++ src/core.clj
@@ -4,16 +4,22 @@
         [net.cgrand.enlive-html :only [html-resource select text]]
         [clojure.contrib.core :only [-?>]])) ;; unused.
 
-(def urls-to-scrape ["http://www.google.com"])
-
+(def num-search-results-to-use 10)
+(def search-terms ["bananas", "blamma"])
 (def selectors [[:#someid]])
+
+(defn google-results [search-term]
+  ["http://www.google.com", "http://www.amazon.com"])
+
+(defn urls-from-search-terms [terms]
+  (mapcat #(take num-search-results-to-use (google-results %)) terms))
 
 (defn url-matches? [url selector]
   (let [html (-> url reader html-resource)]
     (-> html (select selector) seq))) 
   
 (defn -main [& _]
-  (doseq [u urls-to-scrape]
+  (doseq [u (urls-from-search-terms search-terms)]
     (doseq [s selectors]
       (if (url-matches? u s)
         (println (format "'%s' matches selector: %s" u s))

f7e27384194692733f894dbb0a5e7af00a1279f8
Alex Baranosky <alexander.baranosky@gmail.com>
2011-08-14 23:36:20 -0400

    setting up project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject emailclojmatic "0.1"
+  :description "Email List Scraper"
+  :dev-dependencies [[midje "1.3-alpha1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :main core)
---
+++ src/core.clj
@@ -0,0 +1,5 @@
+(ns core
+  (:gen-class))
+  
+(defn -main [& _]
+  (println "Hello World!"))

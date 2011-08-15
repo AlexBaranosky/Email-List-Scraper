781be4734a12db294676422a11d898a9aca8351e
Alex Baranosky <alexander.baranosky@gmail.com>
2011-08-14 23:56:02 -0400

    put in a skeleton of the app

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Email List Scraper"
   :dev-dependencies [[midje "1.3-alpha1"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive "1.0.0"]]
   :main core)
--- src/core.clj
+++ src/core.clj
@@ -1,5 +1,20 @@
 (ns core
-  (:gen-class))
+  (:gen-class)
+  (:use [clojure.contrib.duck-streams :only [reader]]
+        [net.cgrand.enlive-html :only [html-resource select text]]
+        [clojure.contrib.core :only [-?>]])) ;; unused.
+
+(def urls-to-scrape ["http://www.google.com"])
+
+(def selectors [[:#someid]])
+
+(defn url-matches? [url selector]
+  (let [html (-> url reader html-resource)]
+    (-> html (select selector) seq))) 
   
 (defn -main [& _]
-  (println "Hello World!"))
+  (doseq [u urls-to-scrape]
+    (doseq [s selectors]
+      (if (url-matches? u s)
+        (println (format "'%s' matches selector: %s" u s))
+        (println (format "'%s' does not match selector: %s" u s))))))

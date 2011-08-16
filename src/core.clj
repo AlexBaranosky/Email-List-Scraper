c7f9270b0c24bfd7b8e30e78c3c5aeb3ef4aa26c
Alex Baranosky <alexander.baranosky@gmail.com>
2011-08-15 20:25:44 -0400

    wired it up to google. Seems to work. Just need real search terms and real html selectors to make her production ready

--- src/core.clj
+++ src/core.clj
@@ -2,25 +2,35 @@
   (:gen-class)
   (:use [clojure.contrib.duck-streams :only [reader]]
         [net.cgrand.enlive-html :only [html-resource select text]]
-        [clojure.contrib.core :only [-?>]])) ;; unused.
+        [clojure.contrib.http.agent :only [string http-agent]]
+        [clojure.contrib.json :only [read-json]])
+  (import java.net.URLEncoder))
 
-(def num-search-results-to-use 10)
-(def search-terms ["bananas", "blamma"])
-(def selectors [[:#someid]])
+(defn- url-encode [s]
+  (URLEncoder/encode s "UTF-8"))
 
-(defn google-results [search-term]
-  ["http://www.google.com", "http://www.amazon.com"])
+(def num-search-results-to-use 2)
 
-(defn urls-from-search-terms [terms]
-  (mapcat #(take num-search-results-to-use (google-results %)) terms))
+(def search-terms (map url-encode ["banana split", 
+                                   "leopards"]))
+(def selectors [[:#footer] 
+                [:#content]])
 
-(defn url-matches? [url selector]
-  (let [html (-> url reader html-resource)]
-    (-> html (select selector) seq))) 
-  
+(defn- google-url-results-for [search-term]
+  (let [request-url (str "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" search-term)
+        json-response (-> request-url 
+                          http-agent 
+                          string 
+                          (read-json true))]
+    (->> json-response :responseData :results (map :url))))
+
+(defn- urls-from-search-terms [terms]
+  (mapcat #(take num-search-results-to-use (google-url-results-for %)) terms))
+
 (defn -main [& _]
   (doseq [u (urls-from-search-terms search-terms)]
-    (doseq [s selectors]
-      (if (url-matches? u s)
-        (println (format "'%s' matches selector: %s" u s))
-        (println (format "'%s' does not match selector: %s" u s))))))
+    (let [html (-> u reader html-resource)]
+      (doseq [s selectors]
+        (if (-> html (select s) seq)
+          (println (format "'%s' matches selector: %s" u s))
+          (println (format "'%s' does not match selector: %s" u s)))))))
